6a7171a2274058130e907585fb91a64f0229ddb9
Sebastian Döll <sebastian@katallaxie.me>
2023-10-31 17:00:36 +0000

    chore: update job

--- build.ts
+++ build.ts
@@ -7,7 +7,7 @@ import * as path from 'path'
 const data = {
   name: chalk.bold(chalk.whiteBright('           Sebastian Doell')),
   handle: chalk.bold(chalk.whiteBright('katallaxie')),
-  work: chalk.white('Head of PaaS Internal Services at IONOS Cloud'),
+  work: chalk.white('Solution Architect @ ZEISS GROUP'),
   github: chalk.gray('https://github.com/') + chalk.green('katallaxie'),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('katallaxie'),
   mastodon: chalk.gray('https://hachyderm.io/') + chalk.cyan('@katallaxie'),
